e02b6dbde921b14e152790d48aace4f70a8ce7a0
Zachary <zinouye@gmail.com>
2018-05-22 18:19:16 -0700

    cleaned up code and adjusted styling on product entries

--- src/components/related/Entry/relatedProductEntry.jsx
+++ src/components/related/Entry/relatedProductEntry.jsx
@@ -2,16 +2,15 @@ import React from 'react';
 import './relatedProductEntry.css';
 
 const RelatedProductsEntry = (props) => {
-  console.log(props.price);
   let check = props.description.replace(/<(?:.|\n)*?>/gm, '');;
   return (
     <div>
-      <div className="related-left related-middle">
+      <div>
         <img className="related-image" src={props.picture} alt="" />
       </div>
       <div className="related-body">
         <div className="related-title">{props.title}</div>
-        <div className="related-price">{props.price}</div>
+        <div className="related-price">${props.price}</div>
         <div className="related-description">{(check)}</div>
       </div>
     </div>
--- src/components/related/relatedProducts.jsx
+++ src/components/related/relatedProducts.jsx
@@ -1,46 +1,25 @@
 import React, { Component } from 'react';
 import * as data from '../../data/data';
+import './relatedProducts.css';
 import RelatedProductsEntry from './Entry/relatedProductEntry';
-
-// console.log('data', data);
 
 class RelatedProductsComponent extends Component {
   render() {
     let relatedArray = data.fields.relatedProducts
-    console.log('data', relatedArray);
     return (
       <div className="related-list">
-        {relatedArray.map(entry => {
-          console.log(entry);
-          return (<RelatedProductsEntry description ={entry.fields.shopifyProduct.body_html} 
-            title={entry.fields.name} 
-            price={entry.fields.shopifyProduct.variants[0].price}
-            key={entry.fields.shopifyProductId}
-            picture={entry.fields.shopifyProduct.image.src}
-            />)
-        }
-        )}
+        <h2>Related Products</h2>
+          {relatedArray.map(entry => {
+            return (<RelatedProductsEntry description ={entry.fields.shopifyProduct.body_html} 
+              title={entry.fields.name} 
+              price={entry.fields.shopifyProduct.variants[0].price}
+              picture={entry.fields.shopifyProduct.image.src}
+              key={entry.fields.shopifyProductId}
+              />)
+          }
+          )}
       </div>
     )
   }
 }
 export default RelatedProductsComponent;
-
-// class ControlModule extends Component {
-// 	render() {
-// 		let {played, mouseDown, change, mouseUp, loaded} = this.props;
-// 		return (
-// 			<div className="progress-container">
-// 				<input
-// 					type='range' min={0} max={1} step='any'
-// 					value={played}
-// 					onMouseDown={mouseDown}
-// 					onChange={change}
-// 					onMouseUp={mouseUp}
-// 				/>
-// 				<progress className='played' max={1} value={played} />
-// 				<progress className='video-load' max={1} value={loaded} />
-// 			</div>
-// 		);
-// 	}
-// }
